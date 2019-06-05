8735f7830d572d20faa82b40deae66bd21484cf7
Alex Oh <alexhboh@gmail.com>
2019-06-05 07:16:01 -0400

    Commented the code, gem image position and current score needs to be fixed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,33 +1,51 @@
 $(document).ready(function() {
+    // declaring and initializing the variable score, wins and losses.
     var score = 0;
     var wins = 0;
     var losses = 0;
-    var goalScore = Math.floor((Math.random() * 20) + 10);
+    //Score will apear anywhere from 10 - 39
+    var goalScore = Math.floor((Math.random() * 30) + 10);
+
+    // every gem will have a random value 
     var redGemVal = Math.floor((Math.random() * 10) + 1);
     var yellowGemVal = Math.floor((Math.random() * 10) + 1);
     var greenGemVal = Math.floor((Math.random() * 10) + 1);
     var blueGemVal = Math.floor((Math.random() * 10) + 1);
 
+    // showing the goalScore
     $(".scoreGoal").append(goalScore);
 
+    // when a user clicks red Gem
     $(".redGem").on("click", function() {
+    //adds a redGemVal to the current score
     score = score + redGemVal;
+    // prints out the score
     $("#yourScore").html(score);
 
+    //win condition 
     if (score == goalScore){
+        // win count goes up
         wins++;
+        // print win count
         $("#win").html(wins);
+        // resetting the goal score, and the current score.
         goalScore = Math.floor((Math.random() * 20) + 10);
         score = 0;
+        //printing current score back to 0 and a random goalScore.
         $("#yourScore").html(score);
         $(".scoreGoal").html(goalScore);
 
     }
+    // lose condition
     else if (score > goalScore){
+        // loss count goes up
         losses++;
+        // print out loss count
         $("#loss").html(losses);
+       // resetting the goal score, and the current score.
         goalScore = Math.floor((Math.random() * 20) + 10);
         score = 0;
+        //printing current score back to 0 and a random goalScore.
         $("#yourScore").html(score);
         $(".scoreGoal").html(goalScore);
     }
@@ -95,9 +113,6 @@ $(".blueGem").on("click", function() {
         $(".scoreGoal").html(goalScore);
     }
 });
-
-
-
 });
 
 
